c5ea822cadff9ed016c874bf44620c7974aebdf2
Pelagia-Applications <102488035+Pelagia-Applications@users.noreply.github.com>
2026-05-22 16:35:43 -0400

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1143,6 +1143,7 @@
     const key = picker.value;
     if (key && EXAMPLES[key]) {
       editor.value = EXAMPLES[key];
+      editor.dispatchEvent(new Event('input'));
       clearOutput();
       setStatus('Ready', 'ok');
     }
